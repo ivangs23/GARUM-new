f8856a468782141c157f4cf3428b89acadd8ff3f
Ivan Gonzalez Suarez <ivan_gs_20@hotmail.com>
2026-05-05 10:57:10 +0200

    feat(shared): expose ticket module via subpath export

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,3 +3,4 @@ export * from "./domain";
 export * from "./format";
 export * from "./constants";
 export type { Database, Json, Tables } from "./database.types";
+export * from "./ticket";
---
+++ packages/shared/src/ticket/index.ts
@@ -0,0 +1,3 @@
+export * from "./types";
+export * from "./build";
+export { exampleOrder } from "./example-order";
